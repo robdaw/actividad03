190f7a5dbae3ff72c7e726feff451c6fb12161e6
robdaw <cem201327@gmail.com>
2017-10-05 09:08:54 +0200

    actividad03
    entrega

---
+++ adivina.js
@@ -0,0 +1,12 @@
+var numero1=prompt ('Escribe un numero');
+var numero2="12";
+
+if (numero1<numero2) {
+  alert("TE QUEDASTE CORTO")
+}
+  else if (numero1>numero2) {
+    alert("TE PASASTE")
+  }
+  else if (numero1=numero2) {
+      alert("ACERTASTE")
+  }
